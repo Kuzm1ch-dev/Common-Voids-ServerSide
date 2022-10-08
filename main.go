2c0cc2d7d96370a5a37317be84d96f827bad55e6
Kuzm1ch88 <steam.danya@yandex.ru>
2022-10-08 10:18:09 +0300

    handler

--- main.go
+++ main.go
@@ -2,81 +2,98 @@ package main
 
 import (
 	"bufio"
-	"github.com/google/uuid"
-	"go.uber.org/zap"
+	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
-	"sync"
+	"strings"
 )
-
-const (
-	CONN_TYPE = "tcp"
-	CONN_HOST = "localhost"
-	CONN_PORT = "25565"
-)
-
-var connMap = &sync.Map{}
-
-type usersList struct {
-	uid  string
-	conn net.Conn
-}
 
 func main() {
-	var loggerConfig = zap.NewProductionConfig()
-	loggerConfig.Level.SetLevel(zap.DebugLevel)
 
-	logger, err := loggerConfig.Build()
+	const maxUsers = 16 // By default
+
+	users := make(map[net.Conn]string)   // Map of active connections
+	newConnection := make(chan net.Conn) // Handle new connection
+	addedUser := make(chan net.Conn)     // Add new connection
+	deadUser := make(chan net.Conn)      // Users that have left chat
+	messages := make(chan string)        // channel that recieves messages from all users
+
+	server, err := net.Listen("tcp", ":25565")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-	if err != nil {
-		logger.Error("Error listening: ", zap.Error(err))
-		os.Exit(0)
-	}
-	defer l.Close()
-
-	logger.Info("Listening on " + CONN_HOST + ":" + CONN_PORT)
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			logger.Error("Error accepting ", zap.Error(err))
-			continue
+	go func() { // Launch routine that will accept connections
+		for {
+			conn, err := server.Accept()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			if len(users) < maxUsers {
+				newConnection <- conn // Send to handle new user
+			} else {
+				io.WriteString(conn, "Server is full!")
+			}
 		}
-		logger.Info("New connection: " + conn.LocalAddr().String())
-
-		id := uuid.New().String()
-		connMap.Store(id, conn)
-
-		go handleRequest(id, conn, connMap, logger)
-	}
-}
-
-func handleRequest(id string, conn net.Conn, connMap *sync.Map, logger *zap.Logger) {
-	defer func() {
-		conn.Close()
-		connMap.Delete(id)
 	}()
 
-	for {
-		userInput, err := bufio.NewReader(conn).ReadString('\n')
-		logger.Info("MSG:" + userInput)
-		if err != nil {
-			logger.Error("Error reading from client", zap.Error(err))
-			return
-		}
+	for { // Run forever
 
-		connMap.Range(func(key, value interface{}) bool {
-			if conn, ok := value.(net.Conn); ok {
-				if _, err := conn.Write([]byte(userInput)); err != nil {
-					logger.Error("Error on writing to connection", zap.Error(err))
+		select {
+		case conn := <-newConnection:
+
+			go func(conn net.Conn) { // Ask user for name and information
+				reader := bufio.NewReader(conn)
+				io.WriteString(conn, "Enter name: ")
+				userName, _ := reader.ReadString('\n')
+				userName = strings.Trim(userName, "\r\n")
+				log.Printf("Accepted new user : %s", userName)
+				io.WriteString(conn, "200:accept")
+				messages <- fmt.Sprintf("Accepted user : [%s]\n\n", userName)
+
+				users[conn] = userName // Add connection
+
+				addedUser <- conn // Add user to pool
+			}(conn)
+
+		case conn := <-addedUser: // Launch a new go routine for the newly added user
+
+			go func(conn net.Conn, userName string) {
+				reader := bufio.NewReader(conn)
+				for { // Run forever and handle this user's messages
+					newMessage, err := reader.ReadString('\n')
+					newMessage = strings.Trim(newMessage, "\r\n")
+					if err != nil {
+						break
+					}
+					// Send to messages channel therefore ring every user
+					messages <- fmt.Sprintf(">%s: %s \a\n\n", userName, newMessage)
 				}
+
+				deadUser <- conn // If error occurs, connection has been terminated
+				messages <- fmt.Sprintf("%s disconnected\n\n", userName)
+			}(conn, users[conn])
+
+		case message := <-messages: // If message recieved from any user
+
+			for conn, _ := range users { // Send to all users
+				go func(conn net.Conn, message string) { // Write to all user connections
+					_, err := io.WriteString(conn, message)
+					if err != nil {
+						deadUser <- conn
+					}
+				}(conn, message)
+				log.Printf("New message: %s", message)
+				log.Printf("Sent to %d users", len(users))
 			}
 
-			return true
-		})
+		case conn := <-deadUser: // Handle dead users
+			log.Printf("Client disconnected")
+			delete(users, conn)
+		}
 	}
 }
